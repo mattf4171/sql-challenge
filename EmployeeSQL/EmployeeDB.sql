cb6d08cfac69bf7ec761d2d6431c68d03c7fe985
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-04-24 21:43:25 -0700

    Fix Titles Table foreign key err

--- EmployeeSQL/EmployeeDB.sql
+++ EmployeeSQL/EmployeeDB.sql
@@ -1,19 +1,19 @@
 -- Data Engineering
 
 drop table if exists Departments;
+drop table if exists Employees;
 drop table if exists Dept_emp;
 drop table if exists Dept_manager;
-drop table if exists Employees;
 drop table if exists Salaries;
 drop table if exists Titles;
 
 create table Departments (
 	dept_no varchar(4) not null primary key,
-	dept_name varchar(25) not null,
+	dept_name varchar(25) not null
 );
 
 create table Employees (
-	emp_no numeric(6) not null primary key,
+	emp_no varchar(6) not null primary key,
 	emp_title_id varchar(5) not null,
 	birth_date date not null,
 	first_name varchar(25) not null,
@@ -36,17 +36,16 @@ create table Dept_manager (
 	foreign key (dept_no) references Departments(dept_no)
 );
 
-create table Dept_manager (
+create table Salaries (
 	emp_no varchar(6) not null,
-	dept_no varchar(4) not null,
-	foreign key (emp_no) references Employees(emp_no),
-	foreign key (dept_no) references Departments(dept_no)
+	salary numeric(4) not null,
+	foreign key (emp_no) references Employees(emp_no)
 );
 
 create table Titles (
-	title_id varchar(5) not null,
+	emp_title_id varchar(5) not null,
 	title varchar(25) not null,
-	foreign key (title_id) references Employees(emp_title_id)
+	foreign key (emp_title_id) references Employees(emp_title_id)
 );
 
 -- Data Analysis
