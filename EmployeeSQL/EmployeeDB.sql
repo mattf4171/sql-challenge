eb621076fbecaeef195b3a55686db9ada6897131
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-04-25 23:16:05 -0700

    Update queries

--- EmployeeSQL/EmployeeDB.sql
+++ EmployeeSQL/EmployeeDB.sql
@@ -34,26 +34,65 @@ create table Dept_emp (
 	emp_no varchar(6) not null,
 	dept_no varchar(4) not null,
 	foreign key (emp_no) references Employees(emp_no),
-	foreign key (dept_no) references Departments(dept_no)
+	foreign key (dept_no) references Departments(dept_no),
+	primary key (emp_no, dept_no)
 );
 
 create table Dept_manager (
-	emp_no varchar(6) not null,
 	dept_no varchar(4) not null,
+	emp_no varchar(6) not null,
 	foreign key (emp_no) references Employees(emp_no),
 	foreign key (dept_no) references Departments(dept_no)
 );
 
 create table Salaries (
-	emp_no varchar(6) not null,
-	salary numeric(4) not null,
+	emp_no varchar(6) primary key,
+	salary numeric(6) not null,
 	foreign key (emp_no) references Employees(emp_no)
 );
 
 
+copy departments from
+'/private/tmp/sql-challenge-data/departments.csv' 
+csv header delimiter ',';
+
+-- select * from departments;
+
+copy titles from
+'/private/tmp/sql-challenge-data/titles.csv' 
+csv header delimiter ',';
+
+-- select * from titles;
+
+copy employees from
+'/private/tmp/sql-challenge-data/employees.csv' 
+csv header delimiter ',';
+
+-- select * from employees;
+
+copy dept_emp from
+'/private/tmp/sql-challenge-data/dept_emp.csv' 
+csv header delimiter ',';
+
+-- select * from dept_emp;
+
+copy dept_manager from
+'/private/tmp/sql-challenge-data/dept_manager.csv' 
+csv header delimiter ',';
+
+-- select * from dept_manager;
+
+copy salaries from
+'/private/tmp/sql-challenge-data/salaries.csv' 
+csv header delimiter ',';
+
+-- select * from salaries;
 -- Data Analysis
 
 -- List the employee number, last name, first name, sex, and salary of each employee.
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from Employees as e join salaries as s 
+on e.emp_no = s.emp_no;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 
@@ -68,4 +107,3 @@ create table Salaries (
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
-
