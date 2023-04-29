e61cc49b51c6a870c16d13d4522a5cb62cbe5b2e
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-04-29 16:24:00 -0700

    Finalized queries for Data Engineering & Analysis

--- EmployeeSQL/EmployeeDB.sql
+++ EmployeeSQL/EmployeeDB.sql
@@ -55,38 +55,35 @@ create table Salaries (
 copy departments from
 '/private/tmp/sql-challenge-data/departments.csv' 
 csv header delimiter ',';
-
 -- select * from departments;
 
 copy titles from
 '/private/tmp/sql-challenge-data/titles.csv' 
 csv header delimiter ',';
-
 -- select * from titles;
 
 copy employees from
 '/private/tmp/sql-challenge-data/employees.csv' 
 csv header delimiter ',';
-
 -- select * from employees;
 
 copy dept_emp from
 '/private/tmp/sql-challenge-data/dept_emp.csv' 
 csv header delimiter ',';
-
 -- select * from dept_emp;
 
 copy dept_manager from
 '/private/tmp/sql-challenge-data/dept_manager.csv' 
 csv header delimiter ',';
-
 -- select * from dept_manager;
 
 copy salaries from
 '/private/tmp/sql-challenge-data/salaries.csv' 
 csv header delimiter ',';
-
 -- select * from salaries;
+
+
+
 -- Data Analysis
 
 -- List the employee number, last name, first name, sex, and salary of each employee.
@@ -100,14 +97,12 @@ from employees as e
 where Year(e.hire_date) = 1986;
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
--- FIX THIS --
 select Distinct(e.first_name, departments.dept_no, departments.dept_name, e.emp_no, e.last_name, e.first_name)
 from employees as e inner join dept_manager on e.emp_no=dept_manager.emp_no 
 inner join departments on dept_manager.dept_no=departments.dept_no 
 inner join dept_emp on departments.dept_no=dept_emp.dept_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
--- FIX THIS --
 select departments.dept_no, e.emp_no, e.last_name, e.first_name, departments.dept_name
 from employees as e inner join dept_manager on e.emp_no=dept_manager.emp_no 
 inner join departments on dept_manager.dept_no=departments.dept_no 
@@ -125,11 +120,10 @@ inner join departments on dept_manager.dept_no=departments.dept_no
 where departments.dept_name like 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
--- FIX THIS --
-select e.emp_no, e.last_name, e.first_name, departments.dept_no
-from employees as e inner join dept_manager on e.emp_no=dept_manager.emp_no
-inner join departments on dept_manager.dept_no=departments.dept_no
-where departments.dept_name like 'Sales' and departments.dept_name like 'Development';
+select e.emp_no, e.last_name, e.first_name, departments.dept_name
+from employees as e join dept_manager on e.emp_no=dept_manager.emp_no 
+join departments on dept_manager.dept_no=departments.dept_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 select e.last_name, count(distinct(e.last_name))
